4173d44d6f9f36f7549218f9127161e0bc95fad6
cau4alves <cau4alves@gmail.com>
2025-09-15 17:07:00 -0300

    Deu erro vou fazer em casa

--- App.jsx
+++ App.jsx
@@ -1,12 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './screens/Login';
+import Register from './screens/Register';
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <SafeAreaView style={{ flex: 1}}>
-      <Login />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Register" component={Register} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
--- screens/Login.jsx
+++ screens/Login.jsx
@@ -1,12 +1,17 @@
-import { SafeAreaView, TextInput, Text, TouchableOpacity } from 'react-native';
-import * as S from '../style.js'; 
+import { SafeAreaView, TextInput, Text, TouchableOpacity, Button } from 'react-native';
+import * as S from '../style.js';
+import { useNavigation } from '@react-navigation/native'; 
  
 
 export default function Login() {
+  const navigation = useNavigation();
+
   return (
     <S.Container>
         <S.Title>Sign in to your Account</S.Title>
-        <S.SubTitle>Don’t have an account? <Text style={{color: "#4D81E7"}}>Sign Up</Text></S.SubTitle>
+        <S.SubTitle>Don’t have an account? <TouchableOpacity onPress={() => navigation.navigate('Register')}>
+    <Text style={{ color: "#4D81E7" }}>Sign Up</Text>
+  </TouchableOpacity>Sign Up</S.SubTitle>
       
 
         <S.Input1
